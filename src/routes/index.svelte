b868ab7bdd474131c233423730fe45ff47f2782d
Atta <aththabrani.telebort@gmail.com>
2022-01-18 16:20:26 +0000

    uhhh

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+    let activity="";
+    let type="";
+    let participants="";
+    
+    async function getInfo() {
+        fetch('https://www.boredapi.com/api/activity')
+            .then((response) => response.json())
+            .then((data) => {
+                activity = data.activity;
+                type = data.type;
+                participants = data.participants;
+
+            });
+    }
+</script>
+
+<p>Activity: {activity}</p>
+<p>Type: {type}</p>
+<p>Participants: {participants}</p>
+
+
+
